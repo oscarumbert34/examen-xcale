6288338d1f25677ca7b88bb4ce4121a2e3dd6705
umbert oscar <oscar.umbert@gmail.com>
2022-07-06 17:23:19 -0300

    se agrega comentarios

--- src/main/java/xcale/examen/examen/model/Observer.java
+++ src/main/java/xcale/examen/examen/model/Observer.java
@@ -1,6 +1,10 @@
 package xcale.examen.examen.model;
 
 public interface Observer {
+    /**
+     * Este es el metodo en el cual todos los Contactos se enteraran que hay un nuevo mensaje
+     * @param notification
+     */
     void notify(Notification notification);
 
 }
